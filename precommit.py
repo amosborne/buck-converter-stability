69be96c7ab43d7485a78e5d4a770625162e51871
amosborne <amosborne@users.noreply.github.com>
2022-01-09 06:25:55 -0800

    touch up sch

--- precommit.py
+++ precommit.py
@@ -1,6 +1,7 @@
 # Verify there are no unstaged/untracked files under altium/.
 import re
 import subprocess
+import argparse
 
 status = subprocess.check_output("git status --porcelain", encoding="utf8")
 pattern = re.compile("^.(.) (.*)$")
@@ -10,10 +11,15 @@ for line in status.splitlines():
 		if match.group(2).startswith("altium/"):
 			raise UserWarning("Unstaged or untracked file on altium/ path.")
 
+parser = argparse.ArgumentParser()
+parser.add_argument("proj", nargs="?", default=None, help="choose: dcdc, clk")
+args = parser.parse_args()
+
 # 1. Run all Altium output job containers.
 # 2. Format the bill of materials spreadsheet.
-import altium.DCDC_CHANNEL.job.run_all_containers
-import altium.DCDC_CHANNEL.bom.format_bom
-
-import altium.CLK_DISTRO.job.run_all_containers
-import altium.CLK_DISTRO.bom.format_bom
+if args.proj is None or args.proj == "dcdc":
+	import altium.DCDC_CHANNEL.job.run_all_containers
+	import altium.DCDC_CHANNEL.bom.format_bom
+if args.proj is None or args.proj == "clk":
+	import altium.CLK_DISTRO.job.run_all_containers
+	import altium.CLK_DISTRO.bom.format_bom
